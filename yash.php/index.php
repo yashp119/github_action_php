947a7f7f19d6402f962c57e0c41c99abf758f5ab
yashp119 <125194772+yashp119@users.noreply.github.com>
2023-12-27 23:00:11 +0530

    Create index.php

---
+++ yash.php/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>My PHP Page</title>
+</head>
+<body>
+
+<?php
+// This is a PHP comment
+echo "Hello, world! Today is " . date("Y-m-d") . "<br>";
+
+// Displaying a variable
+$name = "John Doe";
+echo "My name is $name.<br>";
+
+// Using a conditional statement
+if (date("H") < 12) {
+    echo "Good morning!<br>";
+} else {
+    echo "Good afternoon or evening!<br>";
+}
+
+// Creating an array
+$fruits = array("apple", "banana", "orange");
+echo "I like " . $fruits[1] . ".<br>";
+
+// Looping through an array
+foreach ($fruits as $fruit) {
+    echo "- $fruit<br>";
+}
+?>
+
+</body>
+</html>
